a85523421ff453c1e87e62947575e59f8469c154
Eivind Uggedal <eivind@uggedal.com>
2023-03-02 22:09:41 +0100

    init: divert customizations

---
+++ init.el
@@ -0,0 +1,5 @@
+;; Divert customaizations to separate file:
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (and custom-file
+           (file-exists-p custom-file))
+  (load custom-file nil 'nomessage))
