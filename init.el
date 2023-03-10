28dce26b923fdc2e685f82ef0e9a0c071d2efdba
Eivind Uggedal <eivind@uggedal.com>
2023-03-10 14:28:32 +0100

    consult: map C-s to consult-line

--- init.el
+++ init.el
@@ -209,6 +209,7 @@
          ([remap switch-to-buffer-other-window] . consult-buffer-other-window)
          ([remap switch-to-buffer-other-frame] . consult-buffer-other-frame)
          ([remap project-switch-to-buffer] . consult-project-buffer)
+         ([remap isearch-forward] . consult-line)
          ([remap goto-line] . consult-goto-line)
          ([remap yank-pop] . consult-yank-pop)))
 
