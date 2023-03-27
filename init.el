9c8a18b0ecd2d71e881fdbebfe6c5e18b61c8837
Eivind Uggedal <eivind@uggedal.com>
2023-03-27 22:04:49 +0200

    magit: tweak magit repo list
    Add dirty column, a wider version column and symbols for pull/push state.

--- init.el
+++ init.el
@@ -385,6 +385,20 @@
 
 (ensure-package 'magit)
 
+(setopt magit-repository-directories `(("~/src" . 1))
+        magit-repolist-columns
+        '(
+          ("Name" 25 magit-repolist-column-ident nil)
+          ("Version" 30 magit-repolist-column-version nil)
+          ("D" 1 magit-repolist-column-dirty nil)
+          ("⇣" 3 magit-repolist-column-unpulled-from-upstream
+           ((:right-align t)
+            (:help-echo "Upstream changes not in branch")))
+          ("⇡" 3 magit-repolist-column-unpushed-to-upstream
+           ((:right-align t)
+            (:help-echo "Local changes not in upstream")))
+          ("Path" 99 magit-repolist-column-path nil)))
+
 (with-eval-after-load 'magit-status
   (eval-when-compile (require 'magit-status))
   ;; Conflicts with MacOS like window switching:
