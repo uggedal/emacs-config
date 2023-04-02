eac2d7956a62475575a5220016219c0ced9d3384
Eivind Uggedal <eivind@uggedal.com>
2023-04-02 21:59:40 +0200

    Revert "init: reformat with elisp-autofmt"
    This reverts commit b73910e0ff94946f5b6ab861c8244d8a7a8bfd5a.

--- init.el
+++ init.el
@@ -15,7 +15,9 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(setq package-archive-priorities '(("melpa" . 3) ("gnu" . 2) ("nongnu" . 1)))
+(setq package-archive-priorities '(("melpa" . 3)
+                                   ("gnu"    . 2)
+                                   ("nongnu" . 1)))
 
 (unless (bound-and-true-p package--initialized)
   (package-initialize))
@@ -47,67 +49,42 @@
 ;;; Core
 ;;;
 
-(setopt
- ring-bell-function
- 'ignore
- use-short-answers
- t
- create-lockfiles
- nil
- ;; Hide commands in M-x not applicable to active mode:
- read-extended-command-predicate
- #'command-completion-default-include-p
- enable-recursive-minibuffers
- t
- ;; Disable right-to-left text:
- bidi-paragraph-direction
- 'left-to-right
- bidi-inhibit-bpa
- t
- ;;TAB first indents then completes:
- tab-always-indent
- 'complete
- ;;Make cursor fill entire tab width:
- x-stretch-cursor
- t
- sentence-end-double-space
- nil
- ;;Make point follow scrolling:
- scroll-conservatively
- 101
- scroll-margin
- 3
- fill-column
- 79
- ;;Dashed indicator line"
- display-fill-column-indicator-character
- ?\u254e
- ;;Do not display line continuation lines:
- truncate-lines
- t
- ;;Show unfinished keystrokes immediately:
- echo-keystrokes
- 1e-6
- initial-scratch-message
- nil
- ;;Don't ask to kill buffers with processes:
- kill-buffer-query-functions
- nil
- history-delete-duplicates
- t
- history-length
- 1000
- ns-alternate-modifier
- nil
- ns-command-modifier
- 'meta)
+(setopt ring-bell-function 'ignore
+        use-short-answers t
+        create-lockfiles nil
+        ;; Hide commands in M-x not applicable to active mode:
+        read-extended-command-predicate
+        #'command-completion-default-include-p
+        enable-recursive-minibuffers t
+        ;; Disable right-to-left text:
+        bidi-paragraph-direction 'left-to-right
+        bidi-inhibit-bpa t
+        ;;TAB first indents then completes:
+        tab-always-indent 'complete
+        ;;Make cursor fill entire tab width:
+        x-stretch-cursor t
+        sentence-end-double-space nil
+        ;;Make point follow scrolling:
+        scroll-conservatively 101
+        scroll-margin 3
+        fill-column 79
+        ;;Dashed indicator line"
+        display-fill-column-indicator-character ?\u254e
+        ;;Do not display line continuation lines:
+        truncate-lines t
+        ;;Show unfinished keystrokes immediately:
+        echo-keystrokes 1e-6
+        initial-scratch-message nil
+        ;;Don't ask to kill buffers with processes:
+        kill-buffer-query-functions nil
+        history-delete-duplicates t
+        history-length 1000
+        ns-alternate-modifier nil
+        ns-command-modifier 'meta)
 
 
-(add-editing-modes-hook
- (lambda ()
-   (setq-local
-    show-trailing-whitespace t
-    indicate-empty-lines t)))
+(add-editing-modes-hook (lambda () (setq-local show-trailing-whitespace t
+                                               indicate-empty-lines t)))
 
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
@@ -126,67 +103,67 @@
 (ensure-package 'modus-themes)
 (require 'modus-themes)
 
-(setopt
- modus-themes-common-palette-overrides
- '(
-   ;; Subtle line numbers:
-   (fg-line-number-active fg-main)
-   (fg-line-number-inactive "gray50")
-   (bg-line-number-active unspecified)
-   (bg-line-number-inactive unspecified)
+(setopt modus-themes-common-palette-overrides
+        '(
+          ;; Subtle line numbers:
+          (fg-line-number-active fg-main)
+          (fg-line-number-inactive "gray50")
+          (bg-line-number-active unspecified)
+          (bg-line-number-inactive unspecified)
 
-   ;; Disable link underline:
-   (underline-link unspecified)
-   (underline-link-visited unspecified)
-   (underline-link-symbolic unspecified)
+          ;; Disable link underline:
+          (underline-link unspecified)
+          (underline-link-visited unspecified)
+          (underline-link-symbolic unspecified)
 
-   ;; Subtle fringe:
-   (fringe unspecified)
+          ;; Subtle fringe:
+          (fringe unspecified)
 
-   ;; Border-less mode line:
-   (border-mode-line-active unspecified)
-   (border-mode-line-inactive unspecified)
+          ;; Border-less mode line:
+          (border-mode-line-active unspecified)
+          (border-mode-line-inactive unspecified)
 
-   ;; Some color for region background:
-   (bg-region bg-yellow-nuanced)
+          ;; Some color for region background:
+          (bg-region bg-yellow-nuanced)
 
-   ;; Some color for active mode line:
-   (bg-mode-line-active bg-blue-subtle)
+          ;; Some color for active mode line:
+          (bg-mode-line-active bg-blue-subtle)
 
-   ;; Normalize heading colors:
-   (fg-heading-1 fg-alt)
-   (fg-heading-2 fg-alt)
-   (fg-heading-3 fg-alt)
-   (fg-heading-4 fg-alt)
-   (fg-heading-5 fg-alt)
-   (fg-heading-6 fg-alt)
-   (fg-heading-7 fg-alt)
-   (fg-heading-8 fg-alt)
+          ;; Normalize heading colors:
+          (fg-heading-1 fg-alt)
+          (fg-heading-2 fg-alt)
+          (fg-heading-3 fg-alt)
+          (fg-heading-4 fg-alt)
+          (fg-heading-5 fg-alt)
+          (fg-heading-6 fg-alt)
+          (fg-heading-7 fg-alt)
+          (fg-heading-8 fg-alt)
 
-   ;; Lighter fringe diff highlights:
-   (bg-added-fringe bg-added)
-   (bg-changed-fringe bg-changed)
-   (bg-removed-fringe bg-removed)))
+          ;; Lighter fringe diff highlights:
+          (bg-added-fringe bg-added)
+          (bg-changed-fringe bg-changed)
+          (bg-removed-fringe bg-removed)
+          ))
 
 (defun modus-themes-custom-faces ()
   "Custom overrides of Modus Operandi theme."
   (modus-themes-with-colors
-   (let ((padding 6))
-     (custom-set-faces
-      ;; Padding for mode line:
-      `(mode-line
-        ((,c :box (:line-width ,padding :color ,bg-mode-line-active))))
-      `(mode-line-inactive
-        ((,c :box (:line-width ,padding :color ,bg-mode-line-inactive))))))
-   (custom-set-faces
-    `(org-todo ((,c :foreground ,red :background ,bg-red-nuanced)))
-    ;; Dim DONE Org headlines:
-    `(org-done ((,c :foreground ,fg-dim)))
-    ;; Bring back dashed fill column indicator (relative height and no bg):
-    `(fill-column-indicator
-      ((,c :height 1.0 :background ,nil :foreground ,bg-active)))
-    ;; Less invasive Eglot highlights:
-    `(eglot-highlight-symbol-face ((,c :background , bg-green-nuanced))))))
+    (let ((padding 6))
+      (custom-set-faces
+       ;; Padding for mode line:
+       `(mode-line
+         ((,c :box (:line-width ,padding :color ,bg-mode-line-active))))
+       `(mode-line-inactive
+         ((,c :box (:line-width ,padding :color ,bg-mode-line-inactive))))))
+    (custom-set-faces
+     `(org-todo ((,c :foreground ,red :background ,bg-red-nuanced)))
+     ;; Dim DONE Org headlines:
+     `(org-done ((,c :foreground ,fg-dim)))
+     ;; Bring back dashed fill column indicator (relative height and no bg):
+     `(fill-column-indicator ((,c :height 1.0 :background ,nil
+                                  :foreground ,bg-active)))
+     ;; Less invasive Eglot highlights:
+     `(eglot-highlight-symbol-face ((,c :background , bg-green-nuanced))))))
 
 (add-hook 'modus-themes-after-load-theme-hook #'modus-themes-custom-faces)
 
@@ -201,7 +178,8 @@
 (ensure-package 'diminish)
 (require 'diminish)
 
-(setopt whitespace-line-column 79 whitespace-style '(face tabs lines-tail))
+(setopt whitespace-line-column 79
+        whitespace-style '(face tabs lines-tail))
 (add-hook 'prog-mode-hook 'whitespace-mode)
 (add-hook 'whitespace-mode-hook (lambda () (diminish 'whitespace-mode)))
 
@@ -215,15 +193,10 @@
 ;;; Editing
 ;;;
 
-(setopt
- column-number-mode
- t
- save-interprogram-paste-before-kill
- t
- kill-do-not-save-duplicates
- t
- indent-tabs-mode
- nil)
+(setopt column-number-mode t
+        save-interprogram-paste-before-kill t
+        kill-do-not-save-duplicates t
+        indent-tabs-mode nil)
 
 (keymap-global-set "M-z" 'zap-up-to-char)
 (keymap-global-set "M-u" 'upcase-dwim)
@@ -249,7 +222,8 @@
 ;;;
 
 
-(setopt make-backup-files nil require-final-newline t)
+(setopt make-backup-files nil
+        require-final-newline t)
 
 (let ((auto-save-dir (concat user-emacs-directory "auto-save/")))
   (setopt auto-save-file-name-transforms `((".*" ,auto-save-dir t)))
@@ -268,13 +242,11 @@
 (keymap-global-set "C-x C-r" 'recentf-open)
 (recentf-mode)
 
-(setopt
- savehist-additional-variables
- '(search-ring
-   regexp-search-ring
-   last-kbd-macro
-   shell-command-history
-   log-edit-comment-ring))
+(setopt savehist-additional-variables '(search-ring
+                                        regexp-search-ring
+                                        last-kbd-macro
+                                        shell-command-history
+                                        log-edit-comment-ring))
 (savehist-mode)
 (save-place-mode)
 (desktop-save-mode)
@@ -301,14 +273,11 @@
 (keymap-global-set "C-x C-b" 'ibuffer-list-buffers)
 
 (with-eval-after-load 'dired
-  (eval-when-compile
-    (require 'dired))
+  (eval-when-compile (require 'dired))
   (keymap-set dired-mode-map "RET" 'dired-find-alternate-file)
-  (keymap-set
-   dired-mode-map "^"
-   (lambda ()
-     (interactive)
-     (find-alternate-file "..")))
+  (keymap-set dired-mode-map "^" (lambda ()
+                                   (interactive)
+                                   (find-alternate-file "..")))
   (setopt dired-listing-switches "-alFh \"-D%Y-%m-%d %H:%M\"")
   (put 'dired-find-alternate-file 'disabled nil))
 
@@ -327,39 +296,34 @@
   (vertico-mode))
 
 (with-eval-after-load 'vertico
-  (eval-when-compile
-    (require 'vertico))
+  (eval-when-compile (require 'vertico))
   (keymap-set vertico-map "RET" 'vertico-directory-enter)
   (keymap-set vertico-map "DEL" 'vertico-directory-delete-char)
   (keymap-set vertico-map "M-DEL" 'vertico-directory-delete-word))
 
 (ensure-package 'orderless)
-(setopt
- completion-styles
- '(orderless basic)
- completion-category-defaults
- nil
- completion-category-overrides
- '((file (styles partial-completion))))
+(setopt completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion))))
 
 (ensure-package 'marginalia)
 (setopt marginalia-max-relative-age 0)
 (with-eval-after-load 'marginalia
-  (advice-add
-   'marginalia--time-absolute
-   :override
-   (lambda (time)
-     (let ((system-time-locale "C"))
-       (format-time-string "%Y-%m-%d %H:%M" time)))))
+  (advice-add 'marginalia--time-absolute
+              :override
+              (lambda (time)
+                (let ((system-time-locale "C"))
+                  (format-time-string "%Y-%m-%d %H:%M" time)))))
 (when (fboundp 'marginalia-mode)
   (marginalia-mode))
 
 (ensure-package 'corfu)
-(setopt corfu-cycle t corfu-echo-delay 0.1 corfu-popupinfo-delay nil)
+(setopt corfu-cycle t
+        corfu-echo-delay 0.1
+        corfu-popupinfo-delay nil)
 
 (with-eval-after-load 'corfu
-  (eval-when-compile
-    (require 'corfu))
+  (eval-when-compile (require 'corfu))
 
   (defun corfu-enable-in-minibuffer ()
     "Enable for M-: and M-!"
@@ -381,11 +345,8 @@
 (keymap-global-set "M-/" 'cape-dabbrev)
 
 (ensure-package 'consult)
-(setopt
- xref-show-xrefs-function
- 'consult-xref
- xref-show-definitions-function
- 'consult-xref)
+(setopt xref-show-xrefs-function 'consult-xref
+        xref-show-definitions-function 'consult-xref)
 (keymap-global-set "M-y" 'consult-yank-pop)
 (keymap-global-set "<remap> <goto-line>" 'consult-goto-line)
 
@@ -395,8 +356,7 @@
 
 (ensure-package 'vterm)
 (with-eval-after-load 'vterm
-  (eval-when-compile
-    (require 'vterm))
+  (eval-when-compile (require 'vterm))
   (keymap-set vterm-mode-map "M-`" nil)
   (keymap-set vterm-mode-map "C-q" 'vterm-send-next-key))
 
@@ -410,7 +370,8 @@
 ;;; VCS
 ;;;
 
-(setopt vc-handled-backends '(Git) log-edit-maximum-comment-ring-size 1000)
+(setopt vc-handled-backends '(Git)
+        log-edit-maximum-comment-ring-size 1000)
 
 (ensure-package 'git-commit)
 
@@ -421,52 +382,45 @@
 (defun commit-message-completion ()
   "Search for previous commit messages from history."
   (interactive)
-  (eval-and-compile
-    (require 'dash)
-    (require 'log-edit))
+  (eval-and-compile (require 'dash)
+                    (require 'log-edit))
 
 
-  (insert
-   (completing-read
-    "History: "
-    (-remove
-     (lambda (item) (string-match-p (regexp-quote "Summary: ") item))
-     (delete-dups
-      ;; Remove unnecessary newlines:
-      (mapcar
-       (lambda (text) (string-trim text))
-       (ring-elements log-edit-comment-ring)))))))
+  (insert (completing-read "History: "
+                           (-remove
+                            (lambda (item)
+                              (string-match-p (regexp-quote "Summary: ") item))
+                            (delete-dups
+                             ;; Remove unnecessary newlines:
+                             (mapcar (lambda (text)
+                                       (string-trim text))
+                                     (ring-elements
+                                      log-edit-comment-ring)))))))
 
 (with-eval-after-load 'git-commit
-  (eval-when-compile
-    (require 'git-commit))
+  (eval-when-compile (require 'git-commit))
   (keymap-set git-commit-mode-map "M-r" 'commit-message-completion))
 
 (ensure-package 'magit)
 
 (setopt magit-repository-directories `(("~/src" . 1)))
 (with-eval-after-load 'magit-repos
-  (setopt
-   magit-repolist-columns
-   '(("Name" 25 magit-repolist-column-ident nil)
-     ("Version" 35 magit-repolist-column-version nil)
-     ("Flag"
-      4
-      magit-repolist-column-flag
-      ((:right-align t) (:help-echo "U[N]tracked [U]nstaged [S]taged")))
-     ("⇣"
-      3
-      magit-repolist-column-unpulled-from-upstream
-      ((:right-align t) (:help-echo "Upstream changes not in branch")))
-     ("⇡"
-      3
-      magit-repolist-column-unpushed-to-upstream
-      ((:right-align t) (:help-echo "Local changes not in upstream")))
-     ("Path" 99 magit-repolist-column-path nil))))
+  (setopt magit-repolist-columns
+          '(("Name" 25 magit-repolist-column-ident nil)
+            ("Version" 35 magit-repolist-column-version nil)
+            ("Flag" 4 magit-repolist-column-flag
+             ((:right-align t)
+              (:help-echo "U[N]tracked [U]nstaged [S]taged")))
+            ("⇣" 3 magit-repolist-column-unpulled-from-upstream
+             ((:right-align t)
+              (:help-echo "Upstream changes not in branch")))
+            ("⇡" 3 magit-repolist-column-unpushed-to-upstream
+             ((:right-align t)
+              (:help-echo "Local changes not in upstream")))
+            ("Path" 99 magit-repolist-column-path nil))))
 
 (with-eval-after-load 'magit-status
-  (eval-when-compile
-    (require 'magit-status))
+  (eval-when-compile (require 'magit-status))
   ;; Conflicts with MacOS like window switching:
   (keymap-set magit-status-mode-map "C-<tab>" nil))
 
@@ -485,15 +439,14 @@
 
   (if (not (buffer-file-name))
       (error "Non-file buffer!"))
-  (cond
-   ((= 0 (length (magit-unstaged-files)))
-    (message "No changes to commit"))
-   (t
-    (vc-git-command nil 0 nil "commit" "-am" "sync")
-    (vc-git-command nil 'async nil "push")
-    (diff-hl-update)
-    (message "Committed and pushed")
-    t)))
+  (cond ((= 0 (length (magit-unstaged-files)))
+         (message "No changes to commit"))
+        (t
+         (vc-git-command nil 0 nil "commit" "-am" "sync")
+         (vc-git-command nil 'async nil "push")
+         (diff-hl-update)
+         (message "Committed and pushed")
+         t)))
 (keymap-global-set "C-x v p" 'automatic-commit-and-push)
 
 (setopt diff-font-lock-prettify t)
@@ -528,8 +481,7 @@
 (setopt eglot-autoshutdown t)
 
 (with-eval-after-load 'eglot
-  (eval-when-compile
-    (require 'eglot))
+  (eval-when-compile (require 'eglot))
 
   (keymap-set eglot-mode-map "C-c l f" 'eglot-format)
   (keymap-set eglot-mode-map "C-c l r" 'eglot-rename)
@@ -569,51 +521,39 @@
 (add-editing-modes-hook 'jinx-mode)
 
 (with-eval-after-load 'jinx
-  (eval-when-compile
-    (require 'jinx))
+  (eval-when-compile (require 'jinx))
 
   ;; Disable for strings:
-  (setopt
-   jinx-include-faces
-   '((prog-mode font-lock-comment-face font-lock-doc-face)
-     (conf-mode font-lock-comment-face)))
+  (setopt jinx-include-faces   '((prog-mode font-lock-comment-face
+                                            font-lock-doc-face)
+                                 (conf-mode font-lock-comment-face)))
 
   (keymap-set jinx-mode-map "C-;" 'jinx-correct))
 
 (setopt calendar-week-start-day 1)
 
-(setopt
- org-hide-emphasis-markers
- t
- org-startup-indented
- t
- org-startup-folded
- 'content
- org-blank-before-new-entry
- '((heading . t) (plain-list-item . nil))
- org-todo-keywords
- '((sequence "TODO" "NEXT" "DOING" "|" "DONE"))
- org-startup-with-inline-images
- t
- org-ellipsis
- " ▾"
- org-image-actual-width
- nil
- org-special-ctrl-a/e
- t
- org-directory
- "~/src/notes"
- org-agenda-files
- '("work.org" "personal.org" "tech.org"))
+(setopt org-hide-emphasis-markers t
+        org-startup-indented t
+        org-startup-folded 'content
+        org-blank-before-new-entry '((heading . t)
+                                     (plain-list-item . nil))
+        org-todo-keywords '((sequence "TODO" "NEXT" "DOING" "|" "DONE"))
+        org-startup-with-inline-images t
+        org-ellipsis " ▾"
+        org-image-actual-width nil
+        org-special-ctrl-a/e t
+        org-directory "~/src/notes"
+        org-agenda-files '("work.org" "personal.org" "tech.org"))
 (with-eval-after-load 'org
-  (eval-when-compile
-    (require 'org))
+  (eval-when-compile (require 'org))
   (keymap-set org-mode-map "C-M-<up>" 'org-up-element))
 
-(setopt
- org-todo-keyword-faces
- '(("NEXT" . '(modus-themes-fg-green modus-themes-nuanced-green org-todo))
-   ("DOING" . '(modus-themes-fg-yellow modus-themes-nuanced-yellow org-todo))))
+(setopt org-todo-keyword-faces '(("NEXT" .
+                                  '(modus-themes-fg-green
+                                    modus-themes-nuanced-green org-todo))
+                                 ("DOING" .
+                                  '(modus-themes-fg-yellow
+                                    modus-themes-nuanced-yellow org-todo))))
 
 (setopt org-cycle-separator-lines 1)
 
@@ -623,14 +563,12 @@
 (keymap-global-set "C-c a" 'org-agenda)
 (add-hook 'org-agenda-mode-hook 'hl-line-mode)
 
-(setopt
- org-capture-templates
- '(("j"
-    "Journal Entry"
-    entry
-    (file+olp+datetree "~/src/notes/work.org")
-    "* %?"
-    :empty-lines 1)))
+(setopt org-capture-templates
+        '(
+          ("j" "Journal Entry"
+           entry (file+olp+datetree "~/src/notes/work.org")
+           "* %?"
+           :empty-lines 1)))
 
 (keymap-global-set "C-c c" 'org-capture)
 (keymap-global-set "C-c l" 'org-store-link)
@@ -678,7 +616,8 @@
 ;;;
 
 (ensure-package 'rainbow-mode)
-(setopt rainbow-html-colors nil rainbow-x-colors nil)
+(setopt rainbow-html-colors nil
+        rainbow-x-colors nil)
 
 ;;;
 ;;; Misc
