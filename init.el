f442c87ed364ccd5cb1627f6175294381f220826
Eivind Uggedal <eivind@uggedal.com>
2023-04-03 18:51:56 +0200

    core: disable dialog boxes

--- init.el
+++ init.el
@@ -51,6 +51,7 @@
 
 (setopt ring-bell-function 'ignore
         use-short-answers t
+        use-dialog-box nil
         create-lockfiles nil
         ;; Hide commands in M-x not applicable to active mode:
         read-extended-command-predicate
