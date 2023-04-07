339a283ced533d8d86283f9c13d0e76d32bbbc1e
Eivind Uggedal <eivind@uggedal.com>
2023-04-07 09:25:53 +0200

    face: reduce size
    Better matching retina setup on 4k monitor

--- init.el
+++ init.el
@@ -171,7 +171,7 @@
 (modus-themes-load-theme 'modus-operandi)
 
 (with-eval-after-load 'faces
-  (set-face-attribute 'default nil :font "SF Mono" :height 140))
+  (set-face-attribute 'default nil :font "SF Mono" :height 120))
 
 (add-prog-and-conf-modes-hook 'display-line-numbers-mode)
 (add-prog-and-conf-modes-hook 'display-fill-column-indicator-mode)
