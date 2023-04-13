f46f23c51c6efd76e3043c29a78aa3af66f6a69a
Eivind Uggedal <eivind@uggedal.com>
2023-04-13 11:33:37 +0200

    theme: nicer tab bar

--- init.el
+++ init.el
@@ -149,19 +149,30 @@
 (defun modus-themes-custom-faces ()
   "Custom overrides of Modus Operandi theme."
   (modus-themes-with-colors
-    (let ((padding 6))
+    (let ((mode-line-padding 6)
+          (tab-bar-padding 4))
       (custom-set-faces
-       ;; Padding for mode line:
+       ;; Padding for mode line and tab-bar:
        `(mode-line
-         ((,c :box (:line-width ,padding :color ,bg-mode-line-active))))
+         ((,c :box
+              (:line-width ,mode-line-padding :color ,bg-mode-line-active))))
        `(mode-line-inactive
-         ((,c :box (:line-width ,padding :color ,bg-mode-line-inactive))))))
+         ((,c :box
+              (:line-width ,mode-line-padding :color ,bg-mode-line-inactive))))
+       `(tab-bar ((,c :background ,bg-tab-current)))
+       `(tab-bar-tab
+         ((,c :box (:line-width ,tab-bar-padding :color ,bg-yellow-nuanced)
+              :background ,bg-yellow-nuanced)))
+       `(tab-bar-tab-inactive
+         ((,c :box (:line-width ,tab-bar-padding :color ,bg-mode-line-inactive)
+              :background ,bg-mode-line-inactive)))
+       ))
     (custom-set-faces
      `(org-todo ((,c :foreground ,red :background ,bg-red-nuanced)))
      ;; Dim DONE Org headlines:
      `(org-done ((,c :foreground ,fg-dim)))
      ;; Bring back dashed fill column indicator (relative height and no bg):
-     `(fill-column-indicator ((,c :height 1.0 :background ,nil
+     `(fill-column-indicator ((,c :height 1.0 :background ,bg-main
                                   :foreground ,bg-active)))
      ;; Less invasive Eglot highlights:
      `(eglot-highlight-symbol-face ((,c :background , bg-green-nuanced))))))
