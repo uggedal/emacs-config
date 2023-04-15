aec5a6cf54578f06878caa3ed9913c563d3c786a
Eivind Uggedal <eivind@uggedal.com>
2023-04-15 18:10:16 +0200

    faces: SF Pro as variable-pitch face

--- init.el
+++ init.el
@@ -182,7 +182,8 @@
 (modus-themes-load-theme 'modus-operandi)
 
 (with-eval-after-load 'faces
-  (set-face-attribute 'default nil :font "SF Mono" :height 120))
+  (set-face-attribute 'default nil :font "SF Mono" :height 120)
+  (set-face-attribute 'variable-pitch nil :font "SF Pro" :height 120))
 
 (add-prog-and-conf-modes-hook 'display-line-numbers-mode)
 (add-prog-and-conf-modes-hook 'display-fill-column-indicator-mode)
