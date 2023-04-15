1c8706c46475da169e282ef9f42a3c9f3e8870fa
Eivind Uggedal <eivind@uggedal.com>
2023-04-15 18:15:40 +0200

    faces: use New York for variable-pitch

--- init.el
+++ init.el
@@ -183,7 +183,7 @@
 
 (with-eval-after-load 'faces
   (set-face-attribute 'default nil :font "SF Mono" :height 120)
-  (set-face-attribute 'variable-pitch nil :font "SF Pro" :height 120))
+  (set-face-attribute 'variable-pitch nil :font "New York" :height 120))
 
 (add-prog-and-conf-modes-hook 'display-line-numbers-mode)
 (add-prog-and-conf-modes-hook 'display-fill-column-indicator-mode)
