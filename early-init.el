483899c7e4814ee1ab90764ec62c146db54161dd
Eivind Uggedal <eivind@uggedal.com>
2023-09-08 13:14:03 +0200

    early-init: setopt and rm path changes

--- early-init.el
+++ early-init.el
@@ -1,31 +1,22 @@
 ;; Minimal GUI:
-(custom-set-variables '(tool-bar-mode . nil)
-                      '(scroll-bar-mode nil))
-
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 (modify-all-frames-parameters '((ns-apperance . light)
                                 (ns-transparent-titlebar . t)
                                 (width . 200)
                                 (height . 60)))
 
 ;; Do not restrict resize to columns and lines:
-(setq frame-resize-pixelwise t)
+(setopt frame-resize-pixelwise t)
 
 ;; No startup screen or echo message:
-(setq inhibit-startup-screen t
-      inhibit-startup-echo-area-message user-login-name)
+(setopt inhibit-startup-screen t
+        inhibit-startup-echo-area-message user-login-name)
 
 ;; No frame resizing when setting font:
-(setq frame-inhibit-implied-resize t)
+(setopt frame-inhibit-implied-resize t)
 
 ;; Disable native compilation warnings:
-(setq native-comp-async-report-warnings-errors 'silent)
+(setopt native-comp-async-report-warnings-errors 'silent)
 
 (setopt load-prefer-newer t)
-
-;; Extend PATH:
-(let ((local_bin (expand-file-name "~/.local/bin")))
-  (setenv "PATH" (concat local_bin ":" (getenv "PATH")))
-  (setq exec-path (push local_bin exec-path)))
-
-;; Extend MANPATH:
-(setenv "MANPATH" (concat "/opt/local/share/man:" (getenv "MANPATH")))
