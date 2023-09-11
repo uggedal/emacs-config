35ae9c41d297aa6d31fe6eb110b06a843680c565
Eivind Uggedal <eivind@uggedal.com>
2023-09-11 10:09:08 +0200

    mode-line: faces

--- init.el
+++ init.el
@@ -163,23 +163,25 @@
   (set-face-attribute 'fixed-pitch nil :font "SF Mono")
   (set-face-attribute 'variable-pitch nil :font "New York"))
 
-(setopt mode-line-format
-        '("%e" mode-line-front-space
-          (:propertize
-           (""
-            mode-line-mule-info
-            mode-line-client
-            mode-line-modified
-            mode-line-remote)
-           display (min-width (5.0)))
-          mode-line-frame-identification
-          mode-line-buffer-identification
-          "   "
-          mode-line-position (vc-mode vc-mode)
-          mode-line-format-right-align
-          mode-line-modes
-          mode-line-misc-info
-          mode-line-end-spaces))
+(use-package emacs
+  :init
+  (setopt mode-line-format
+          '("%e" mode-line-front-space
+            (:propertize
+             (""
+              mode-line-mule-info
+              mode-line-client
+              mode-line-modified
+              mode-line-remote)
+             display (min-width (5.0)))
+            mode-line-frame-identification
+            mode-line-buffer-identification
+            "   "
+            mode-line-position (vc-mode vc-mode)
+            mode-line-format-right-align
+            mode-line-modes
+            mode-line-misc-info
+            mode-line-end-spaces)))
 
 (use-package display-line-numbers
   :hook ((prog-mode conf-mode) . display-line-numbers-mode))
