f399f7bf0fcc1bdb5915377e54813f286454c359
Eivind Uggedal <eivind@uggedal.com>
2025-06-19 09:55:07 +0200

    init: remove all config except minimal gui and font

--- early-init.el
+++ early-init.el
@@ -1,10 +1,6 @@
 ;; Minimal GUI:
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(modify-all-frames-parameters '((ns-apperance . light)
-                                (ns-transparent-titlebar . t)
-                                (width . 200)
-                                (height . 60)))
 
 ;; Do not restrict resize to columns and lines:
 (setopt frame-resize-pixelwise t)
@@ -12,11 +8,3 @@
 ;; No startup screen or echo message:
 (setopt inhibit-startup-screen t
         inhibit-startup-echo-area-message user-login-name)
-
-;; No frame resizing when setting font:
-(setopt frame-inhibit-implied-resize t)
-
-;; Disable native compilation warnings:
-(setopt native-comp-async-report-warnings-errors 'silent)
-
-(setopt load-prefer-newer t)
