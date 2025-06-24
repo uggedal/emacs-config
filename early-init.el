7f3fcb953c24cbe94f16bc6e1317cade01745559
Eivind Uggedal <eivind@uggedal.com>
2025-06-24 10:52:38 +0200

    early-init: 200 col and 60 col initial window size

--- early-init.el
+++ early-init.el
@@ -2,6 +2,10 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
+;; Nice initial window size:
+(modify-all-frames-parameters '((width . 200)
+				(height . 60)))
+
 ;; Do not restrict resize to columns and lines:
 (setopt frame-resize-pixelwise t
 	frame-inhibit-implied-resize t)
